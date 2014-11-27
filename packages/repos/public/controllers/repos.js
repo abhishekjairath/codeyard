5d25be917a588802f747d9d2fb25ed720c0d5c32
Shubhank Srivastava <shubhanksrivastava94@gmail.com>
2014-11-27 13:59:56 +0530

    Commit views changed

--- packages/repos/public/controllers/repos.js
+++ packages/repos/public/controllers/repos.js
@@ -235,44 +235,6 @@ angular.module('mean.repos').controller('ReposController', ['$scope', 'Global', 
         });
     };
 
-}]).controller('EditorController', ['$scope', 'Global', 'Repos','$stateParams',
-  function($scope, Global, Repos, $stateParams) {
-    $scope.file = $stateParams.repo;
-    $scope.init = function() {
-      var firepadRef = getExampleRef();
-      // Create CodeMirror (with lineWrapping on).
-      var codeMirror = CodeMirror(document.getElementById('firepad'), { lineNumbers:true,lineWrapping: true, mode:'javascript'});
-      // Create a random ID to use as our user ID (we must give this to firepad and FirepadUserList).
-      //var userId = Math.floor(Math.random() * 9999999999).toString();
-      // Create Firepad (with rich text features and our desired userId).
-      var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror,
-          { richTextToolbar: false, richTextShortcuts: false});
-      // Create FirepadUserList (with our desired userId).
-      //var firepadUserList = FirepadUserList.fromDiv(firepadRef.child('users'),document.getElementById('userlist'), userId);
-      // Initialize contents.
-      firepad.on('ready', function() {
-        if (firepad.isHistoryEmpty()) {
-            firepad.setText('Check out the user list to the left!');
-        }
-      });
-    }
-
-    function getExampleRef() {
-      var ref = new Firebase('https://codeyard.firebaseIO.com');
-      var hash = window.location.hash.replace(/#!\/edit/g, '');
-      if (hash) {
-        ref = ref.child(hash);
-      } else {
-        ref = ref.push(); // generate unique location.
-        window.location = window.location + '#' + ref.key(); // add it as a hash to the URL.
-      }
-      if (typeof console !== 'undefined')
-        console.log('Firebase data: ', ref.toString());
-      return ref;
-    }
-
-
-
 }]).controller('WikiController', ['$scope', 'Global', 'Repos',
   function($scope, Global, Repos) {
     
@@ -317,15 +279,12 @@ angular.module('mean.repos').controller('ReposController', ['$scope', 'Global', 
     $scope.viewCommit = function(){
         Repos.viewCommit($stateParams.id).success(function(data){
             $scope.commit = data.response;
-            console.log($scope.commit.changes);
             for(var i=0;i<$scope.commit.changes.length;i++){
                 for(var j=0;j<$scope.commit.changes[i].updations.length;j++){
                     if($scope.commit.changes[i].updations[j].status==0){
                         $scope.additions++;
-                        
                     }else if($scope.commit.changes[i].updations[j].status==1){
                         $scope.deletions++;
-                        
                     }else if($scope.commit.changes[i].updations[j].status==2){
                         $scope.additions++;
                     }
